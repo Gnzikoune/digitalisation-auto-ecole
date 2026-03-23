1d833a858bd560e6ffacd287eb3594f0701b5773
Gnzikoune <gnzikoune@gmail.com>
2026-03-23 17:30:23 +0100

    fix(examens): correction de l'injection du CandidateRepository

--- backend/src/modules/examens/examens.module.ts
+++ backend/src/modules/examens/examens.module.ts
@@ -4,9 +4,10 @@ import { ExamensService } from './examens.service';
 import { ExamensController } from './examens.controller';
 import { Exam } from './entities/exam.entity';
 import { ExamResult } from './entities/exam-result.entity';
+import { Candidate } from '../candidats/entities/candidate.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Exam, ExamResult])],
+  imports: [TypeOrmModule.forFeature([Exam, ExamResult, Candidate])],
   controllers: [ExamensController],
   providers: [ExamensService],
 })
